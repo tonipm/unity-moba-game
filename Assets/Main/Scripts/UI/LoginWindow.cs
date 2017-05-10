93182f9e1cfa65d8adc7f22aeb75df944432409b
Toni\ToniPM <toniperairam@gmail.com>
2017-05-10 19:23:11 +0200

    Revert "Subida"
    This reverts commit a67b5ddc3eeaeead1787217f6b86e355d4a2a2df.

--- Assets/Main/Scripts/UI/LoginWindow.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class LoginWindow : MonoBehaviour {
-
-    //public Dropdown teamSelector;
-}
--- Assets/Main/Scripts/UI/RoomData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class RoomData : MonoBehaviour {
-
-    public Text roomName;
-    public Text numPlayers;
-    public Button btnEntrar;
-}
